7f9811311e1e7ec21e52386b263256222e1c2f8e
Teruhiro Kuniyasu <t.kuniyasu.24.5.1463@4nonome.com>
2024-06-18 12:14:58 +0900

    間違い

--- euclid.py
+++ euclid.py
@@ -6,15 +6,13 @@ if a > b:
     while r != 0:
         r = a %b
         if r != 0:
-            a = b
-            b = r    
+            a,b = b,r    
     GCD = b
 else:
     while r != 0:
         r = b %a
         if r != 0:
-            b = a
-            a = r
+            b,a = a,r
     GCD = a
 
 print("GCD = ", GCD)
