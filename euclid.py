a40a557f56d440381ddc3f8f2671f1cf9f1472af
Teruhiro Kuniyasu <t.kuniyasu.24.5.1463@4nonome.com>
2024-06-25 10:55:59 +0900

    06/25

--- euclid.py
+++ euclid.py
@@ -1,18 +1,9 @@
+def GCD(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
 a = int(input("a の値を入力: "))
 b = int(input("b の値を入力: "))
-r = 1
 
-if a > b:
-    while r != 0:
-        r = a %b
-        if r != 0:
-            a,b = b,r    
-    GCD = b
-else:
-    while r != 0:
-        r = b %a
-        if r != 0:
-            b,a = a,r
-    GCD = a
-
-print("GCD = ", GCD)
+print("GCD = ", GCD(a, b))
