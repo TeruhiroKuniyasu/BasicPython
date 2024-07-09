b34b16526cf6377da6727b8f1835d6915c2300a0
Teruhiro Kuniyasu <t.kuniyasu.24.5.1463@4nonome.com>
2024-07-09 19:27:20 +0900

    07/09

--- [確認課題]numpy.py
+++ [確認課題]numpy.py
@@ -15,9 +15,25 @@ def y(x):
 
 t = y(x) + noise
 
+x0 = np.ones_like(x)
+x1 = x
+x2 = x**2
+
+X = np.vstack((x0, x1, x2)).T
+
+# 回帰係数を計算
+w = np.linalg.inv(X.T @ X) @ X.T @ t
+w0, w1, w2 = w
+
+# 予測値を計算
+pred_y = w0 + w1 * x + w2 * x ** 2
+
 # プロット
 plt.scatter(x, t)
+plt.plot(x, pred_y, linewidth=2, color="red", label="Pred")
+plt.plot(x, y(x), linewidth=2, color="green", label="True")
+plt.legend()
 plt.xlabel('x')
 plt.ylabel('t')
-plt.title('Scatter plot of x vs t')
+plt.title('Scatter plot with true and predicted lines')
 plt.show()
